e7001f090b8adf161de6bd0ba38ff765864352ec
gayathri-kayapati <gayathrikayapati@gmail.com>
2022-11-19 14:34:48 +0530

    feat:added show score functionality

--- index.js
+++ index.js
@@ -71,3 +71,10 @@ function game() {
     movieQuiz(questions[i].question, questions[i].answer);
   }
 }
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+  console.log(
+    "Check out the high scores, if you should be there ping me and I'll update it"
+  );
+  highScores.map((score) => console.log(score.name, " : ", score.score));
+}
